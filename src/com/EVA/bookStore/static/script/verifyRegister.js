7d6bb6895c6ef8291800f9442271eb565d376b93
bookstoreTest <bookStore2020@protonmail.com>
2021-10-14 00:56:25 +0800

    验证密码
    验证密码合法

--- src/com/EVA/bookStore/static/script/verifyRegister.js
+++ src/com/EVA/bookStore/static/script/verifyRegister.js
@@ -10,14 +10,16 @@ $(function () {
         if (!usernamePatt.test(usernameText)) {
             //4 提示用户结果
             //层次选择器 后代元素选择器
-            $("span.errorMsg").text("用户不合法");
+            $("span.errorMsg").text("用户名不合法");
             return false;
         }
+        //细节：有时候表单被推送到一个远程网址，这时，虽然用户名合法，但是提示信息未改变
+        //整个页面停留在老页面中，因此，应该清空错误信息再提交
         $("span.errorMsg").text();
 
         //验证密码：必须由字母，数字，下划线组成，并且长度为5到12位
         //1 获取密码输入框的内容
-        var usernameText = $("#username").val();
+        var usernameText = $("#password").val();
         //2 创建验证的正则表达式
         var usernamePatt = /^\w{5,12}$/;
         //3 使用test方法验证
