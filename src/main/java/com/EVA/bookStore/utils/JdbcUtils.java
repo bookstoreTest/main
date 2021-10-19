104c9a01006d5008bf6067132e88df3aa28330e3
bookstoreTest <bookStore2020@protonmail.com>
2021-10-19 23:03:44 +0800

    创建数据库连接工具类
    创建数据库连接，关闭的工具类JdbcUtils，并引入德鲁伊jar包

--- src/main/java/com/EVA/bookStore/utils/JdbcUtils.java
+++ src/main/java/com/EVA/bookStore/utils/JdbcUtils.java
@@ -1,4 +1,26 @@
 package com.EVA.bookStore.utils;
 
+import java.net.ConnectException;
+import java.sql.Connection;
+
+/**
+ * 创建和关闭数据库
+ */
 public class JdbcUtils {
+    /**
+     * 获取数据库中的连接
+     * @return
+     */
+    public static Connection getConnection(){
+        return null;
+    }
+
+    /**
+     * 释放数据库连接
+     * @param connection
+     */
+    public static void close(Connection connection){
+
+    }
+
 }
