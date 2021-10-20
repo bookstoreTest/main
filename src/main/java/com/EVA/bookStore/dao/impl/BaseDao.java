255ef51289917d5bfb798c5302169dcc7db42eae
bookstoreTest <bookStore2020@protonmail.com>
2021-10-20 20:51:25 +0800

    新增数据库操作接口

--- src/main/java/com/EVA/bookStore/dao/impl/BaseDao.java
+++ src/main/java/com/EVA/bookStore/dao/impl/BaseDao.java
@@ -1,8 +1,85 @@
 package com.EVA.bookStore.dao.impl;
 
+import com.EVA.bookStore.utils.JdbcUtils;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 public abstract class BaseDao {
     //使用JDBCUtils操作数据库
-    private QueryRunner queryRunner;
+    private QueryRunner queryRunner = new QueryRunner();
+
+    /**
+     * 用于在数据库中执行Insert/Update/Delete语句
+     *
+     * @return
+     */
+    public int update(String sql, Object... args) {
+        Connection connection = JdbcUtils.getConnection();
+        try {
+            return queryRunner.update(connection, sql, args);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.close(connection);
+        }
+        return -1;
+
+    }
+
+    /**
+     * 查询返回单个JavaBean的SQL语句
+     * @param type
+     * @param sql
+     * @param args
+     * @param <T>
+     * @return
+     */
+    public <T> T queryForOne(Class<T> type, String sql, Object... args) {
+        Connection connection = JdbcUtils.getConnection();
+        try {
+            return queryRunner.query(connection, sql, new BeanHandler<T>(type), args);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.close(connection);
+        }
+        return null;
+    }
+
+    /**
+     * 查询返回多个JavaBean的语句
+     * @param type
+     * @param sql
+     * @param args
+     * @param <T>
+     * @return
+     */
+    public <T> List<T> queryForList(Class<T> type, String sql, Object... args) {
+        Connection connection = JdbcUtils.getConnection();
+        try {
+            return queryRunner.query(connection, sql, new BeanListHandler<T>(type), args);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.close(connection);
+        }
+        return null;
+    }
+    public Object queryForSingleValue(String sql, Object... args){
+        Connection connection = JdbcUtils.getConnection();
+        try {
+            queryRunner.query(connection,sql,new ScalarHandler(),args);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            JdbcUtils.close(connection);
+        }
+        return -1;
+    }
 }
