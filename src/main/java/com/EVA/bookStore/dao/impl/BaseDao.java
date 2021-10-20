881390f8436790853d4d0d8cc68b432af17d1ec2
bookstoreTest <bookStore2020@protonmail.com>
2021-10-20 20:27:34 +0800

    添加commons-dbutils依赖

---
+++ src/main/java/com/EVA/bookStore/dao/impl/BaseDao.java
@@ -0,0 +1,8 @@
+package com.EVA.bookStore.dao.impl;
+
+import org.apache.commons.dbutils.QueryRunner;
+
+public abstract class BaseDao {
+    //使用JDBCUtils操作数据库
+    private QueryRunner queryRunner;
+}
--- src/test/java/utilsTest/JdbcUtilsTest.java
+++ src/test/java/utilsTest/JdbcUtilsTest.java
@@ -3,9 +3,16 @@ package utilsTest;
 import com.EVA.bookStore.utils.JdbcUtils;
 import org.junit.Test;
 
+import java.sql.Connection;
+
 public class JdbcUtilsTest {
     @Test
     public void testJdbcUtils(){
-        System.out.println(JdbcUtils.getConnection());
+        for (int i=0;i<100;i++){
+            Connection connection = JdbcUtils.getConnection();
+            System.out.println(JdbcUtils.getConnection());
+            JdbcUtils.close(connection);
+        }
+
     }
 }
