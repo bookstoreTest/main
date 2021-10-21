1b04e7f0d2f51bc8e68125be55024f50e8c8e183
bookstoreTest <bookStore2020@protonmail.com>
2021-10-21 19:33:24 +0800

    编写用户服务接口

---
+++ src/main/java/com/EVA/bookStore/service/UserService.java
@@ -0,0 +1,25 @@
+package com.EVA.bookStore.service;
+
+import com.EVA.bookStore.pojo.User;
+
+public interface UserService {
+    /**
+     * 注册用户
+     * @param user
+     */
+    public void registerUser(User user);
+
+    /**
+     * 用户登录
+     * @param user
+     * @return
+     */
+    public User login(User user);
+
+    /**
+     * 检查用户名是否存在
+     * @param username
+     * @return true表示存在，false表示用户名可用
+     */
+    public boolean existsUsername(String username);
+}
