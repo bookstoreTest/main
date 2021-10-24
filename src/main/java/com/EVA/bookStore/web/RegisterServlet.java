eca633b61156c7ddab61ed58ddb3d6b477c6a8be
bookstoreTest <bookStore2020@protonmail.com>
2021-10-24 09:04:11 +0800

    实现用户注册功能

--- src/main/java/com/EVA/bookStore/web/RegisterServlet.java
+++ src/main/java/com/EVA/bookStore/web/RegisterServlet.java
@@ -1,6 +1,51 @@
 package com.EVA.bookStore.web;
 
+import com.EVA.bookStore.dao.UserDao;
+import com.EVA.bookStore.dao.impl.UserDaoImpl;
+import com.EVA.bookStore.pojo.User;
+import com.EVA.bookStore.service.UserService;
+import com.EVA.bookStore.service.impl.UserServiceImpl;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class RegisterServlet extends HttpServlet {
+    private UserService userService = new UserServiceImpl();
+
+    /**
+     * Get请求会导致用户的账号和密码显示在浏览器的地址栏上，不安全
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //1.获取请求参数,根据标签的name属性获取
+        String username  = req.getParameter("username");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+        String code = req.getParameter("code");
+        //2.对比验证码。注意：若在前端校验，用户若在浏览器中修改代码，可以绕过验证码校验，产生安全隐患
+        //因此，验证码在后端校验
+        if("abcd".equalsIgnoreCase(code)){
+            //2.2检查用户名是否可用
+            if(userService.existsUsername(username)==false){
+                //可以注册
+               userService.registerUser(new User(null,username,password,email));
+               req.getRequestDispatcher("/pages/user/regist_success.html").forward(req,resp);
+
+            }else {
+                //跳回注册页面
+                req.getRequestDispatcher("pages/user/regist.html").forward(req,resp);
+            }
+
+        }else {
+            //跳回注册页面
+            req.getRequestDispatcher("pages/user/regist.html").forward(req,resp);
+        }
+    }
 }
